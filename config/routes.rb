e7416f1a441e8384675d3701dc7eed432d9f5d98
uavpliyc <uavpliyc@gmail.com>
2021-02-04 04:04:12 +0000

    [update]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  resources :edit_users
+
+  get 'about' => 'homes#about'
   root to: 'homes#top'
+
 end
